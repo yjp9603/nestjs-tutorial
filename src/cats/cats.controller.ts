d2f75befa9c10ab86c161e494b06926ee865ecc3
Jaden <yjp9603@gmail.com>
2022-10-24 23:34:27 +0900

    Exception filter ~ Interceptors

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,38 +1,57 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { SuccessInterceptor } from './../common/interceptors/success.interceptor';
+import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt-pipe';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
+@UseInterceptors(SuccessInterceptor)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {} //의존성 주입
 
-  // cats/
+  // cats
   @Get('/')
   getAllCat() {
-    return 'all cat';
+    return { cats: 'get all cat api' };
   }
   // cats/:id
   @Get(':id')
-  getOneCat() {
-    return 'one cat';
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    console.log(param);
+    // console.log(typeof param);
+
+    return 'get one cat api';
   }
 
   @Post()
   createCat() {
-    return 'create cat';
+    return 'create cat api';
   }
 
   @Put(':id')
   updateCat() {
-    return 'update cat';
+    return 'update cat api';
   }
 
   @Patch(':id')
   updatePartialCat() {
-    return ' update';
+    return 'update partial cat api';
   }
 
   @Delete(':id')
   deleteCat() {
-    return 'delete service';
+    return 'delete cat api';
   }
 }
